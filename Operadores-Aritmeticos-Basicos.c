a4fcecddc80bdf43b0d05fe0a8aa79e6d79dc3bc
Eduardaregateiro <61891053+Eduardaregateiro@users.noreply.github.com>
2025-03-26 20:20:35 +0000

    inclusão de programa

--- Operadores-Aritmeticos-Basicos.c
+++ Operadores-Aritmeticos-Basicos.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main(){
+    int n1, n2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf ("entre com o numero 1: \n");
+    scanf("%d", &n1);
+    printf ("entre com o numero 2: \n");
+    scanf("%d", &n2);
+    // soma
+    soma = n1 + n2;
+
+    // subtração
+    subtracao = n1 - n2;
+
+    // multiplicação
+    multiplicacao = n1 * n2;
+
+    // divisão
+    divisao = n1 / n2;
+
+    printf("a soma é: %d\n", soma);
+    printf("a subtracao é: %d\n", subtracao);
+    printf("a multiplicacao é: %d\n", multiplicacao);
+    printf("a divisao é: %d\n", divisao);
+
+    return 0;
+
+}
